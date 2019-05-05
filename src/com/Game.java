9e9e6fd71a913079b9a872cbcc6bfc5098c70962
Kuba Włodarczyk <kubw19@gmail.com>
2019-05-05 13:39:17 +0200

    Uproszczony kod

--- src/com/Game.java
+++ src/com/Game.java
@@ -1,84 +1,47 @@
 package com;
 
 import java.awt.*;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.DataBufferInt;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.*;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends JPanel implements KeyListener {
 
-    public static int width = 300;
-    public static int height = width / 16 * 9;
-    public static int scale = 3;
 
-    private JFrame frame;
-    private Thread thread;
     private boolean running = false;
 
-    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+    private int licznik =0;
+    private int licznik2 =0;
 
     public Game(){
-         Dimension size = new Dimension(width * scale, height * scale);
-         setPreferredSize(size);
-
-         frame = new JFrame();
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        System.out.println("duaasd");
     }
 
-    public synchronized void start(){
-        thread = new Thread(this, "Display");
-        thread.start();
-        running = true;
-    }
-
-    public synchronized void stop(){
-        running = false;
-        try {
-            thread.join();
-        } catch(InterruptedException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void run(){
-     while(running){
-            update();
-            render();
-     }
-    }
-
-    public void update(){
-
-    }
-    public void render(){
-        BufferStrategy bs = getBufferStrategy();
-        if(bs == null){
-           createBufferStrategy(3);
-           return;
-        }
-
-        Graphics gr = bs.getDrawGraphics();
+    public void paint(Graphics gr){
+        super.paint(gr);//czyszczenie panelu
+        //kwadrat niebieski
         gr.setColor(Color.BLUE);
-        gr.fillRect(0,0,getWidth(), getHeight());
-        gr.dispose();
-        bs.show();
+        gr.fillRect(licznik,licznik2,10, 10);
 
-
+        //gr.dispose();
     }
 
-    public static void main(String[] args){
 
-        Game g = new Game();
-        g.frame.setResizable(false);
-        g.frame.setTitle("Gra");
-        g.frame.add(g);
-        g.frame.pack();
-        g.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
-        g.frame.setLocationRelativeTo(null);
-        g.frame.setVisible(true);
+    @Override
+    public void keyTyped(KeyEvent e) { }
 
-        g.start();
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT)licznik+=2;
+        if(e.getKeyCode() == KeyEvent.VK_LEFT)licznik-=2;
+        if(e.getKeyCode() == KeyEvent.VK_UP)licznik2-=2;
+        if(e.getKeyCode() == KeyEvent.VK_DOWN)licznik2+=2;
+        repaint();
     }
+
+    @Override
+    public void keyReleased(KeyEvent e) { }
 }
---
+++ src/com/Window.java
@@ -0,0 +1,30 @@
+package com;
+
+import javax.swing.*;
+
+public class Window {
+
+    public static int width = 300;
+    public static int height = width / 16 * 9;
+    public static int scale = 3;
+
+    public JFrame frame;
+
+    public Window(Game g){
+        frame = new JFrame();
+        frame.setResizable(false);
+        frame.setTitle("Gra");
+        frame.add(g);
+        frame.pack();
+        frame.setBounds(0, 0, width * scale, height * scale);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+
+        Game g = new Game();
+        Window window = new Window(g);
+    }
+}
